5ebdfc3ea27b8fbd495963b34f3a15ad4124cd2c
Martin Valgur <martin.valgur@gmail.com>
2020-05-08 20:21:18 +0300

    fix moveWeights.sh

--- gimp-plugins/moveWeights.sh
+++ gimp-plugins/moveWeights.sh
@@ -1,11 +1,12 @@
-unzip weights.zip
+unzip weights.zip 'weights/*'
 mkdir -p CelebAMask-HQ/MaskGAN_demo/checkpoints/label2face_512p
-mkdir -p mkdir monodepth2/models/mono+stereo_640x192
-mkdir pytorch-SRResNet/model
+mkdir -p monodepth2/models/mono+stereo_640x192
+mkdir -p pytorch-SRResNet/model
+mkdir -p deeplabv3
 
 mv weights/colorize/* neural-colorization/
 mv weights/deblur/* DeblurGANv2/
-mv weights/deeplabv3/* deeplabv3
+mv weights/deeplabv3/* deeplabv3/
 mv weights/facegen/* CelebAMask-HQ/MaskGAN_demo/checkpoints/label2face_512p/
 mv weights/faceparse/* face-parsing.PyTorch/
 mv weights/monodepth/* monodepth2/models/mono+stereo_640x192/
