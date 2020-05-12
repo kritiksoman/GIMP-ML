3a8df192bc76e3d161716e7b82d52af4facdf850
kritiksoman <33869270+kritiksoman@users.noreply.github.com>
2020-05-12 11:09:18 +0530

    Update installGimpML-mac.sh

--- gimp-plugins/installGimpML-mac.sh
+++ gimp-plugins/installGimpML-mac.sh
@@ -10,21 +10,12 @@ if [ ! -d "gimpenv" ]; then
 	pip install numpy
 	pip install future
 	pip install torch
-	pip install matplotlib
 	pip install scipy
 	pip install scikit-image
 	pip install typing
-	pip install albumentations
 	pip install enum
 	pip install pretrainedmodels
-	cwd=$(pwd)
-	echo -e "baseLoc='${cwd}/'\n$(cat colorize.py)" > colorize.py
-	echo -e "baseLoc='${cwd}/'\n$(cat deblur.py)" > deblur.py
-	echo -e "baseLoc='${cwd}/'\n$(cat deeplabv3.py)" > deeplabv3.py
-	echo -e "baseLoc='${cwd}/'\n$(cat facegen.py)" > facegen.py
-	echo -e "baseLoc='${cwd}/'\n$(cat faceparse.py)" > faceparse.py
-	echo -e "baseLoc='${cwd}/'\n$(cat monodepth.py)" > monodepth.py
-	echo -e "baseLoc='${cwd}/'\n$(cat super_resolution.py)" > super_resolution.py
+
 	
 	deactivate
 
