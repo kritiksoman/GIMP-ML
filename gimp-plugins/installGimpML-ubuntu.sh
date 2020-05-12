13e96f276243c8a9c7066befd13f2fcda946fc46
kritiksoman <33869270+kritiksoman@users.noreply.github.com>
2020-05-12 11:09:36 +0530

    Update installGimpML-ubuntu.sh

--- gimp-plugins/installGimpML-ubuntu.sh
+++ gimp-plugins/installGimpML-ubuntu.sh
@@ -10,22 +10,11 @@ if [ ! -d "gimpenv" ]; then
 	python2 -m pip install numpy
 	python2 -m pip install future
 	python2 -m pip install torch
-	python2 -m pip install matplotlib
 	python2 -m pip install scipy
 	python2 -m pip install scikit-image
 	python2 -m pip install typing
-	python2 -m pip install albumentations
 	python2 -m pip install enum
 	python2 -m pip install pretrainedmodels
-	cwd=$(pwd)
-	echo -e "baseLoc='${cwd}/'\n$(cat colorize.py)" > colorize.py
-	echo -e "baseLoc='${cwd}/'\n$(cat deblur.py)" > deblur.py
-	echo -e "baseLoc='${cwd}/'\n$(cat deeplabv3.py)" > deeplabv3.py
-	echo -e "baseLoc='${cwd}/'\n$(cat facegen.py)" > facegen.py
-	echo -e "baseLoc='${cwd}/'\n$(cat faceparse.py)" > faceparse.py
-	echo -e "baseLoc='${cwd}/'\n$(cat monodepth.py)" > monodepth.py
-	echo -e "baseLoc='${cwd}/'\n$(cat super_resolution.py)" > super_resolution.py
-	
 	deactivate
 
 	echo "\n-----------Installed GIMP-ML------------\n"
