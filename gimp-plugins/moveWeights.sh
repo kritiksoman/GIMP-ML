607c01ed12ca229b81dcf1854359126d5b5e09fd
Martin Valgur <martin.valgur@gmail.com>
2020-05-14 02:04:55 +0300

    remove unnecessary weight extractions

--- gimp-plugins/moveWeights.sh
+++ gimp-plugins/moveWeights.sh
@@ -1,14 +1,4 @@
 unzip weights.zip 'weights/*'
 mkdir -p CelebAMask-HQ/MaskGAN_demo/checkpoints/label2face_512p
-mkdir -p monodepth2/models/mono+stereo_640x192
-mkdir -p pytorch-SRResNet/model
-mkdir -p deeplabv3
-
-mv weights/colorize/* neural-colorization/
-mv weights/deblur/* DeblurGANv2/
-mv weights/deeplabv3/* deeplabv3/
 mv weights/facegen/* CelebAMask-HQ/MaskGAN_demo/checkpoints/label2face_512p/
-mv weights/faceparse/* face-parsing.PyTorch/
-mv weights/monodepth/* monodepth2/models/mono+stereo_640x192/
-mv weights/super_resolution/* pytorch-SRResNet/model/
 rm -rf weights/
