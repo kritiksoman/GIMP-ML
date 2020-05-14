00671bec042c37e8fdd71d6edf7a2c6bf7c62107
Martin Valgur <martin.valgur@gmail.com>
2020-05-14 03:24:05 +0300

    load neural-colorization using Torch Hub format

--- gimp-plugins/colorize.py
+++ gimp-plugins/colorize.py
@@ -1,98 +1,79 @@
-import os
-import sys
-
-from _util import add_gimpenv_to_pythonpath, baseLoc
+from _util import add_gimpenv_to_pythonpath
 
 add_gimpenv_to_pythonpath()
-modelDir = os.path.join(baseLoc, 'neural-colorization')
-sys.path.append(modelDir)
 
 from gimpfu import *
-import torch
-from model import generator
-from torch.autograd import Variable
-from scipy.ndimage import zoom
-from argparse import Namespace
 import numpy as np
-from skimage.color import rgb2yuv,yuv2rgb
+import torch
+import torch.hub
+from scipy.ndimage import zoom
+from skimage.color import yuv2rgb
 
+
+@torch.no_grad()
 def getcolor(input_image):
-    p = np.repeat(input_image, 3, axis=2)
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-    if torch.cuda.is_available():
-        g_available=1
-    else:
-        g_available=-1
+    G = torch.hub.load('valgur/neural-colorization:pytorch', 'generator',
+                       pretrained=True, map_location=device)
+    G.to(device)
 
-    args=Namespace(model=os.path.join(modelDir, 'model.pth'), gpu=g_available)
-
-    G = generator()
-
-    if torch.cuda.is_available():
-        G=G.cuda()
-        G.load_state_dict(torch.load(args.model))
-    else:
-        G.load_state_dict(torch.load(args.model,map_location=torch.device('cpu')))
-
-    
-    img_yuv = rgb2yuv(p)
-    H,W,_ = img_yuv.shape
-    infimg = np.expand_dims(np.expand_dims(img_yuv[...,0], axis=0), axis=0)
-    img_variable = Variable(torch.Tensor(infimg-0.5))
-    if args.gpu>=0:
-        img_variable=img_variable.cuda(args.gpu)
+    input_image = input_image[..., 0]
+    H, W = input_image.shape
+    infimg = input_image[None, None, ...].astype(np.float32) / 255.
+    img_variable = torch.from_numpy(infimg) - 0.5
+    img_variable = img_variable.to(device)
     res = G(img_variable)
-    uv=res.cpu().detach().numpy()
-    uv[:,0,:,:] *= 0.436
-    uv[:,1,:,:] *= 0.615
-    (_,_,H1,W1) = uv.shape
-    uv = zoom(uv,(1,1,float(H)/H1,float(W)/W1))
-    yuv = np.concatenate([infimg,uv],axis=1)[0]
-    rgb=yuv2rgb(yuv.transpose(1,2,0))
-    out=(rgb.clip(min=0,max=1)*255)[:,:,[0,1,2]]
-   
-    out=out.astype(np.uint8)
-    
+    uv = res.cpu().numpy()
+    uv[:, 0, :, :] *= 0.436
+    uv[:, 1, :, :] *= 0.615
+    _, _, H1, W1 = uv.shape
+    uv = zoom(uv, (1, 1, float(H) / H1, float(W) / W1))
+    yuv = np.concatenate([infimg, uv], axis=1)[0].transpose(1, 2, 0)
 
-    return out
+    rgb = yuv2rgb(yuv * 255)
+    rgb = rgb.clip(0, 255).astype(np.uint8)
+    return rgb
 
-def channelData(layer):#convert gimp image to numpy
-    region=layer.get_pixel_rgn(0, 0, layer.width,layer.height)
-    pixChars=region[:,:] # Take whole layer
-    bpp=region.bpp
-    # return np.frombuffer(pixChars,dtype=np.uint8).reshape(len(pixChars)/bpp,bpp)
-    return np.frombuffer(pixChars,dtype=np.uint8).reshape(layer.height,layer.width,bpp)
 
-def createResultLayer(image,name,result):
-    rlBytes=np.uint8(result).tobytes();
-    rl=gimp.Layer(image,name,image.width,image.height,image.active_layer.type,100,NORMAL_MODE)
-    region=rl.get_pixel_rgn(0, 0, rl.width,rl.height,True)
-    region[:,:]=rlBytes
-    image.add_layer(rl,0)
+def channelData(layer):  # convert gimp image to numpy
+    region = layer.get_pixel_rgn(0, 0, layer.width, layer.height)
+    pixChars = region[:, :]  # Take whole layer
+    bpp = region.bpp
+    return np.frombuffer(pixChars, dtype=np.uint8).reshape(layer.height, layer.width, bpp)
+
+
+def createResultLayer(image, name, result):
+    rlBytes = np.uint8(result).tobytes()
+    rl = gimp.Layer(image, name, image.width, image.height, image.active_layer.type, 100, NORMAL_MODE)
+    region = rl.get_pixel_rgn(0, 0, rl.width, rl.height, True)
+    region[:, :] = rlBytes
+    image.add_layer(rl, 0)
     gimp.displays_flush()
 
-def genNewImg(name,layer_np):
-    h,w,d=layer_np.shape
-    img=pdb.gimp_image_new(w, h, RGB)
-    display=pdb.gimp_display_new(img)
 
-    rlBytes=np.uint8(layer_np).tobytes();
-    rl=gimp.Layer(img,name,img.width,img.height,RGB,100,NORMAL_MODE)
-    region=rl.get_pixel_rgn(0, 0, rl.width,rl.height,True)
-    region[:,:]=rlBytes
+def genNewImg(name, layer_np):
+    h, w, d = layer_np.shape
+    img = pdb.gimp_image_new(w, h, RGB)
+    display = pdb.gimp_display_new(img)
+
+    rlBytes = np.uint8(layer_np).tobytes()
+    rl = gimp.Layer(img, name, img.width, img.height, RGB, 100, NORMAL_MODE)
+    region = rl.get_pixel_rgn(0, 0, rl.width, rl.height, True)
+    region[:, :] = rlBytes
 
     pdb.gimp_image_insert_layer(img, rl, None, 0)
     gimp.displays_flush()
 
-def colorize(img, layer) :
+
+def colorize(img, layer):
     gimp.progress_init("Coloring " + layer.name + "...")
 
     imgmat = channelData(layer)
-    cpy=getcolor(imgmat)
+    cpy = getcolor(imgmat)
 
-    genNewImg(layer.name+'_colored',cpy)
+    genNewImg(layer.name + '_colored', cpy)
 
-    
 
 register(
     "colorize",
@@ -102,8 +83,9 @@ register(
     "Your",
     "2020",
     "colorize...",
-    "*",      # Alternately use RGB, RGB*, GRAY*, INDEXED etc.
-    [   (PF_IMAGE, "image", "Input image", None),
+    "*",  # Alternately use RGB, RGB*, GRAY*, INDEXED etc.
+    [
+        (PF_IMAGE, "image", "Input image", None),
         (PF_DRAWABLE, "drawable", "Input drawable", None),
     ],
     [],
