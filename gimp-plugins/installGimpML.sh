028a05613ad47fd292235cfadd5e5e3b4514ee97
kritiksoman <33869270+kritiksoman@users.noreply.github.com>
2020-05-23 05:16:59 -0400

    new install script for mac, ubuntu and debian

---
+++ gimp-plugins/installGimpML.sh
@@ -0,0 +1,44 @@
+if [ ! -d "gimpenv" ]; then
+
+	echo "\n-----------Installing GIMP-ML-----------\n"
+	
+	if [ "$(uname)" == "Linux" ]; then
+		if [[ $(lsb_release -rs) == "18.04" ]]; then #for ubuntu 18.04
+			sudo apt install python-minimal
+			alias python='python2'
+		       
+		elif [[ $(lsb_release -rs) == "20.04" ]]; then #for ubuntu 20.04
+			sudo apt install python2-minimal
+			wget https://bootstrap.pypa.io/get-pip.py 
+			alias python='python2'
+			python get-pip.py	
+
+		elif [[ $(lsb_release -rs) == "10" ]]; then #for debian 10
+			wget https://bootstrap.pypa.io/get-pip.py 
+			python get-pip.py
+		fi
+	fi
+
+	python -m pip install --user virtualenv
+	python -m virtualenv gimpenv
+	source gimpenv/bin/activate
+	python -m pip install torchvision
+	python -m pip install opencv-python
+	python -m pip install numpy
+	python -m pip install future
+	python -m pip install torch
+	python -m pip install scipy
+	python -m pip install typing
+	python -m pip install enum
+	python -m pip install pretrainedmodels
+	deactivate
+
+	echo "\n-----------Installed GIMP-ML------------\n"
+
+else
+
+	echo "Environment already setup!"
+
+fi
+
+
