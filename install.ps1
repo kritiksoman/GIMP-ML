4ccd29898afa78a2d1323470cab269bb9ecb885a
Martin Valgur <martin.valgur@gmail.com>
2020-07-20 02:08:09 +0300

    register plugins dir automatically in Windows

--- install.ps1
+++ install.ps1
@@ -8,4 +8,25 @@ pip install -r requirements.txt
 python -c "import sys; print(f'python3_executable = r\'{sys.executable}\'')" | out-file -encoding utf8 plugins/_config.py
 conda deactivate
 
+# Register the plugins directory in GIMP settings
+$pluginsDir = (split-path -parent $MyInvocation.MyCommand.Definition) + '\plugins'
+$gimp = (dir "C:\Program Files\GIMP 2\bin\gimp-console-*.exe").FullName
+if (!($gimp -and (Test-Path $gimp))) {
+    throw "Could not find GIMP. Is it installed? You will have to add '$pluginsDir' to Preferences -> Folders -> Plug-ins manually."
+}
+$version = (& $gimp --version | Select-String -Pattern 2\.\d+).Matches.Value
+if (!($version)) {
+    throw "Could not determine GIMP version."
+}
+$gimprcPath = ($env:APPDATA + '\GIMP\' + $version + '\gimprc')
+$escapedDir = [regex]::escape($pluginsDir)
+if (!(Test-Path $gimprcPath) -or !(Select-String -Path $gimprcPath -Pattern 'plug-in-path' -Quiet)) {
+    (cat $gimprcPath) + ('(plug-in-path "${gimp_dir}\\plug-ins;${gimp_plug_in_dir}\\plug-ins;' + $escapedDir + '")') | Set-Content $gimprcPath
+} else {
+    if (!(Select-String -Path $gimprcPath -Pattern ([regex]::escape($escapedDir)) -Quiet))
+    {
+        (cat $gimprcPath) -replace '\(\s*plug-in-path\s+"', ('$0' + $escapedDir + ';') | Set-Content $gimprcPath
+    }
+}
+
 echo "-----------Installed GIMP-ML------------"
