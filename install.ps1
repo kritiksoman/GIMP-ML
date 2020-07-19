38af18ab72fefd98818f606cea5bd5858f6a2748
Martin Valgur <martin.valgur@gmail.com>
2020-07-19 18:17:08 +0300

    use mamba for conda setup
    Mamba handles dependencies and "resolving environment" more robustly.

--- install.ps1
+++ install.ps1
@@ -1,8 +1,9 @@
 echo "-----------Installing GIMP-ML-----------"
 
-conda create -y -n gimpenv python=3
+conda install -y -c conda-forge mamba
+mamba create -y -n gimpenv python=3
 conda activate gimpenv
-conda install -y pytorch torchvision cudatoolkit numpy matplotlib-base -c pytorch
+mamba install -y pytorch torchvision cudatoolkit numpy matplotlib-base -c pytorch
 pip install -r requirements.txt
 python -c "import sys; print(f'python3_executable = r\'{sys.executable}\'')" | out-file -encoding utf8 plugins/_config.py
 conda deactivate
