740e152cdc0e07d043cec694c6fb523f883dafd5
DESKTOP-F04AGRR\Kritik Soman <kritiksoman@gmail.com>
2021-06-07 21:58:35 -0700

    code update

--- install.bat
+++ install.bat
@@ -4,7 +4,7 @@
     python -m pip install virtualenv
 	python -m virtualenv gimpenv3
 	if "%1"=="gpu" (gimpenv3\Scripts\python.exe -m pip install torch==1.8.1+cu102 torchvision==0.9.1+cu102 -f https://download.pytorch.org/whl/torch_stable.html) else (gimpenv3\Scripts\python.exe -m pip install torch==1.8.1+cpu torchvision==0.9.1+cpu -f https://download.pytorch.org/whl/torch_stable.html)
-    gimpenv3\Scripts\python.exe -m pip install GIMP3-ML-pip\.
+    gimpenv3\Scripts\python.exe -m pip install GIMP-ML\.
     gimpenv3\Scripts\python.exe -c "import gimpml; gimpml.setup_python_weights()"
 	echo **** GIMP-ML Setup Ended ****
     exit /b
@@ -21,7 +21,7 @@ fi #
 python -m pip install virtualenv
 python -m virtualenv gimpenv3 #
 source gimpenv3/bin/activate #
-python -m pip install GIMP3-ML-pip/.
+python -m pip install GIMP-ML/.
 python -c "import gimpml; gimpml.setup_python_weights()"
 deactivate #
 echo '*** GIMP-ML Setup Ended ****'
